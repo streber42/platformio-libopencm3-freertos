075ad8f80f03a113a97fa580b212d750697ea990
Sean Murphy <murphy@halfast.com>
2022-02-20 23:01:04 -0800

    working double blink

--- src/main.c
+++ src/main.c
@@ -20,8 +20,9 @@ void vApplicationStackOverflowHook(
 static void task1(void *args __attribute__((unused))) {
     
     for (;;) {
-		gpio_toggle(GPIOC, GPIO13);
-		vTaskDelay(pdMS_TO_TICKS(1000));
+		gpio_toggle(GPIOC, GPIO7);
+		gpio_toggle(GPIOC, GPIO6);
+		vTaskDelay(pdMS_TO_TICKS(250));
 	}
 }
 
@@ -30,20 +31,30 @@ static void task1(void *args __attribute__((unused))) {
  */
 int main(void) {
     // Setup main clock, using external 8MHz crystal 
-    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+    rcc_clock_setup_in_hse_25mhz_out_72mhz();
 
     // Enable clock for GPIO channel C
     rcc_periph_clock_enable(RCC_GPIOC);
-
+	// short delay
+	// for (int i = 0; i < 10000; i++) {
+	//   __asm__("nop");
+	// }
+	
     // Set pinmode for PC13
 	gpio_set_mode(
 		GPIOC,
 		GPIO_MODE_OUTPUT_2_MHZ,
 		GPIO_CNF_OUTPUT_PUSHPULL,
-		GPIO13);
+		GPIO7);
+	gpio_set_mode(
+		GPIOC,
+		GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_PUSHPULL,
+		GPIO6);
 
 	// Turn LED off
-	gpio_set(GPIOC, GPIO13);
+	gpio_set(GPIOC, GPIO7);
+	gpio_clear(GPIOC,GPIO6);
 
     // Tell FreeRTOS about our toggle task, and set it's stack and priority
 	xTaskCreate(task1, "LED", 100, NULL, 2, NULL);
